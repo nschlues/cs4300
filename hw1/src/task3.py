9d89410b3bf4c337ca72ebe88ed4b30e1526e95f
Nathan Schluessler <nathan@keymaker.info>
2026-02-17 17:08:07 +0000

    Starting work on tasks

---
+++ hw1/src/task3.py
@@ -0,0 +1,13 @@
+## Control Structures
+## Nathan Schluessler
+# If statement
+def if_statement(num):
+    if(num > 0):
+        print('Number is positive')
+    elif(num < 0):
+        print('Number is negative')
+    else:
+        print('Number is zero')
+
+# For loop
+def for_loop()
