3b53c3bb2ca483f0bf33aa428c65b90f4b603954
Nathan Schluessler <nathan@keymaker.info>
2026-02-18 06:30:46 +0000

    Finished final testing - included missing test for task 5

--- hw1/tests/test_task5.py
+++ hw1/tests/test_task5.py
@@ -1,6 +1,11 @@
-from task5 import books, student_ids
+from task5 import books, student_ids, get_first_three_books
 
 def test_books():
+    assert isinstance(books, list)
+
+def test_get_first_three_books():
+    assert get_first_three_books(books) == ['Brandon Sanderson: Mistborn', 'N. D. Wilson: Ashtown Burials', 'God: The Bible']
     
 
 def student_ids():
+    assert isinstance(student_ids, dict)
