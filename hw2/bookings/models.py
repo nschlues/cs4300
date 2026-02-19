59d4060133f797dbd4b5d4a1cc49706ce2c95d9e
Nathan Schluessler <nathan@keymaker.info>
2026-02-19 00:44:40 +0000

    3.2 in progress

--- hw2/bookings/models.py
+++ hw2/bookings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Book Model
+class Movie:
+    title = models.Charfield(max_length = 100)
+    , description, release date, duration
+
+
+# Seat Model
+class Model:
+    seat number, booking status
+
+
+# Booking Model
+class Booking:
+    movie, seat, user, booking date
