98ecc757f79b7ca9ea54f383aed04a3a9a87e24a
Nathan Schluessler <nathan@keymaker.info>
2026-03-02 15:10:28 +0000

    3.3 still in progress

--- hw2/bookings/views.py
+++ hw2/bookings/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie 
 
-# SeatViewSet
+# Seat View
 def SeatViewSet(request, ):
 
-
+# Booking View 
 def BookingViewSet(request, movie, user):
 
     return render(request, 'book_seats.html', {'movie'})
+
+# Movie View
+def MovieViewSet():
+    
