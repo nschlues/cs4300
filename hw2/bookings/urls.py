88a195f4c5912acac2127a0df2b9451a6742eced
Nathan Schluessler <nathan@keymaker.info>
2026-03-03 21:25:39 +0000

    Updating urls and views

--- hw2/bookings/urls.py
+++ hw2/bookings/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.BaseViewSet, name="base"),
-    path("/api/movies", views.MovieViewSet , name="movies"),
-    path("/api/seats", views.SeatViewSet , name="seats"),
-    path("/api/bookings", views.BookingViewSet , name="bookings"),
+    path("movies/", views.MovieViewSet , name="movies"),
+    path("seats/", views.SeatViewSet , name="seats"),
+    path("bookings/", views.BookingViewSet , name="bookings"),
+    path("book/<int:movie_id>/", views.SeatViewSet, name="book_seat"),
 ]
--- hw2/bookings/views.py
+++ hw2/bookings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Movie 
+from .models import Movie, Seat, Booking 
 
 # Base View
 def BaseViewSet(request):
